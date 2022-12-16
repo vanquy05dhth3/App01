4776ded90c2cae42ed509919451a4bda0fc3a011
Quy Nguyen Van <quynv@imt-soft.com>
2022-12-16 17:48:29 +0700

    Add project files.

---
+++ App01/Data/App01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using App01.Models;
+
+namespace App01.Data
+{
+    public class App01Context : DbContext
+    {
+        public App01Context (DbContextOptions<App01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<App01.Models.Movie> Movie { get; set; } = default!;
+    }
+}
